93ba469d6208503424a757de32e240d417f474c1
Chii Yuen <chii.yuen@hotmail.com>
2026-06-16 23:54:38 +0800

    fix: remove dynamic sbpf-assembler import

--- packages/core/src/bytecode.ts
+++ packages/core/src/bytecode.ts
@@ -1,16 +1,11 @@
+import { assemble } from "@blueshift-gg/sbpf-assembler";
+
 import type { SbpfArch } from "./config.js";
 
 type CompileAssemblyToBytecode = {
   assemblySource: string;
   arch: SbpfArch;
 };
-
-type SbpfAssembler = {
-  assemble: (source: string, arch: number) => Uint8Array;
-  default?: () => Promise<unknown>;
-};
-
-let assemblerPromise: Promise<SbpfAssembler> | undefined;
 
 /**
  * Compile an assembly source string to a bytecode Uint8Array.
@@ -21,19 +16,5 @@ export async function compileAssemblyToBytecode({
   assemblySource,
   arch,
 }: CompileAssemblyToBytecode): Promise<Uint8Array> {
-  const { assemble } = await loadAssembler();
   return assemble(assemblySource, arch === "v3" ? 3 : 0);
-}
-
-async function loadAssembler(): Promise<SbpfAssembler> {
-  assemblerPromise ??= import("@blueshift-gg/sbpf-assembler").then(async (assembler) => {
-    const maybeInit = assembler.default as unknown;
-    if (typeof maybeInit === "function") {
-      await maybeInit();
-    }
-
-    return assembler as unknown as SbpfAssembler;
-  });
-
-  return assemblerPromise;
 }
