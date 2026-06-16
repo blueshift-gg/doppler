aca3d170ccc8371109f0309517e2a47c2556fec6
Chii Yuen <chii.yuen@hotmail.com>
2026-06-16 20:58:55 +0800

    chore: add rolldown config platform

--- packages/cli/rolldown.config.ts
+++ packages/cli/rolldown.config.ts
@@ -22,4 +22,5 @@ export default defineConfig({
     entryFileNames: "[name].js",
     cleanDir: true,
   },
+  platform: "node",
 });
--- packages/core/rolldown.config.ts
+++ packages/core/rolldown.config.ts
@@ -25,4 +25,5 @@ export default defineConfig({
     entryFileNames: "[name].js",
     cleanDir: true,
   },
+  platform: "neutral",
 });
